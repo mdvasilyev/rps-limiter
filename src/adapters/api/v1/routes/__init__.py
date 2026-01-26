308e07c06a76618bf5b292713171565d65989ee0
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-26 17:48:16 +0300

    fix: rm endpoints

--- src/adapters/api/v1/routes/__init__.py
+++ src/adapters/api/v1/routes/__init__.py
@@ -1,10 +1,6 @@
 """API v1 routes module."""
 
-from src.adapters.api.v1.routes.endpoint import logs_processor_router
-
-api_router_list = [
-    logs_processor_router,
-]
+api_router_list = []
 
 __all__ = [
     "api_router_list",
--- src/adapters/api/v1/routes/endpoint/__init__.py
+++ src/adapters/api/v1/routes/endpoint/__init__.py
@@ -1,7 +1,3 @@
 """API v1 routes endpoint module."""
 
-from .logs_processor import logs_processor_router
-
-__all__ = [
-    "logs_processor_router",
-]
+__all__ = []
