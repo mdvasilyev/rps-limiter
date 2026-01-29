537f4d6094dd44a20e170e1cb90929b1cdd567a8
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-29 22:59:00 +0300

    fix: HTTPXProvider

--- src/core/configurations/httpx/client.py
+++ src/core/configurations/httpx/client.py
@@ -1,12 +1,8 @@
-from typing import AsyncIterator
-
 from dishka import Provider, Scope, provide
 from httpx import AsyncClient
 
 
 class HTTPXProvider(Provider):
     @provide(scope=Scope.APP)
-    async def new_connection(self) -> AsyncIterator[AsyncClient]:
-        conn = AsyncClient()
-        yield conn
-        await conn.aclose()
+    def new_connection(self) -> AsyncClient:
+        return AsyncClient()
