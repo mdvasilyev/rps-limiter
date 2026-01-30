68d7fa769feefa9d4eb0bb7f4d0a2c112e8ccd6e
Maxim Vasilyev <mayxis@inbox.ru>
2026-01-30 15:58:51 +0300

    upd: fetch_and_process worker

--- src/application/workers/fetch_and_process.py
+++ src/application/workers/fetch_and_process.py
@@ -2,7 +2,7 @@ import asyncio
 
 from faststream import FastStream
 from faststream.rabbit import RabbitBroker, RabbitRouter
-from httpx import AsyncClient
+from httpx import AsyncClient, ConnectError
 from loguru import logger
 
 from src.application.services import DecisionMaker
@@ -13,17 +13,14 @@ from src.application.services.service_clients import (
     ModelRegistryClient,
     NotificatorClient,
 )
-from src.application.services.service_clients.prometheus import PrometheusClient
 from src.core.configurations.config import GlobalConfig
 from src.core.configurations.dishka import container
-from src.domain.dto.book import Unbook, WarnUnbooking
-from src.domain.dto.scale import ScaleDown, ScaleUp
+from src.domain.dto import ModelIncreaseDTO, ModelRpsDTO, Scale, Unbook, WarnUnbooking
 
 config = container.get(GlobalConfig)
 
 broker: RabbitBroker = container.get(RabbitBroker)
-app = FastStream(broker)
-router = RabbitRouter()
+router: RabbitRouter = RabbitRouter()
 broker.include_router(router)
 
 
@@ -35,28 +32,46 @@ async def handle_logs_signal(message: dict):
         message.get("triggered_at"),
     )
 
-    model_registry_client = container.get(ModelRegistryClient)
-    model_dispatcher_client = container.get(ModelDispatcherClient)
-    notificator_client = container.get(NotificatorClient)
-    booking_client = container.get(BookingClient)
+    model_registry_client: ModelRegistryClient = container.get(ModelRegistryClient)
+    model_dispatcher_client: ModelDispatcherClient = container.get(
+        ModelDispatcherClient
+    )
+    notificator_client: NotificatorClient = container.get(NotificatorClient)
+    booking_client: BookingClient = container.get(BookingClient)
 
-    active_models = await model_registry_client.find_all_running_models()
-    if not active_models:
-        logger.warning("No active models found")
+    try:
+        active_models: list[dict] = (
+            await model_registry_client.find_all_running_models()
+        )
+        if not active_models:
+            logger.warning("No active models found")
+            return
+    except ConnectError as exc:
+        logger.error("Connection error while finding all running models: {}", exc)
         return
 
     model_load_monitor: ModelLoadMonitor = container.get(ModelLoadMonitor)
 
-    period: int = 1
-    rps_stats = await model_load_monitor.get_current_rps_per_model(period)
-    increase_stats = await model_load_monitor.get_increase_per_model(period)
+    period = 1
+    try:
+        rps_stats: list[ModelRpsDTO] = (
+            await model_load_monitor.get_current_rps_per_model(period)
+        )
+        increase_stats: list[ModelIncreaseDTO] = (
+            await model_load_monitor.get_increase_per_model(period)
+        )
+    except ConnectError as exc:
+        logger.error("Connection error while getting rps data: {}", exc)
+        return
 
-    rps_by_model = {m.model_name: m.rps for m in rps_stats}
-    increase_by_model = {m.model_name: m.requests for m in increase_stats}
+    rps_by_model: dict[str, float] = {m.model_name: m.rps for m in rps_stats}
+    increase_by_model: dict[str, float] = {
+        m.model_name: m.requests for m in increase_stats
+    }
 
-    decision_maker = container.get(DecisionMaker)
+    decision_maker: DecisionMaker = container.get(DecisionMaker)
 
-    actions = decision_maker.process(
+    actions: list[Scale | WarnUnbooking | Unbook] = decision_maker.process(
         active_models=active_models,
         rps_by_model=rps_by_model,
         increase_by_model=increase_by_model,
@@ -68,25 +83,30 @@ async def handle_logs_signal(message: dict):
 
     for action in actions:
         match action:
-            case ScaleUp(model):
-                logger.info("Scaling UP {}", model)
-                await model_dispatcher_client.scale_up(model)
+            case Scale(model_id, replicas):
+                logger.info("Scaling {}", model_id)
+                await model_dispatcher_client.scale(model_id, replicas)
 
-            case ScaleDown(model):
-                logger.info("Scaling DOWN {}", model)
-                await model_dispatcher_client.scale_down(model)
+            case WarnUnbooking(model_id, user_id):
+                logger.info("Warn unbooking {}", model_id)
+                await notificator_client.notify(
+                    model_id, user_id, {"warning unbooking": model_id}
+                )
 
-            case WarnUnbooking(model):
-                logger.warning("Warn unbooking {}", model)
-                await notificator_client.warn_unbooking(model)
-
-            case Unbook(model):
-                logger.warning("Unbooking {}", model)
-                await booking_client.unbook_all(model)
-                await notificator_client.notify_unbook(model)
+            case Unbook(model_id, model_name, user_id):
+                reservations: dict = await booking_client.get_reservations(
+                    model_name=model_name, user_id=user_id
+                )
+                reservation_id: str = reservations.get("id")
+                logger.info("Unbooking {}", reservation_id)
+                await booking_client.delete_reservation(reservation_id)
+                await notificator_client.notify(
+                    model_id, user_id, {"unbooking": model_id}
+                )
 
 
 async def main():
+    app = FastStream(broker)
     try:
         await app.run()
     finally:
