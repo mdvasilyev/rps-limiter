cfcc4c132a06ca6043ce63c656d23da58f315e12
Maxim Vasilyev <mayxis@inbox.ru>
2026-02-01 20:19:43 +0300

    refactor: app launch

--- src/application/main.py
+++ src/application/main.py
@@ -1,16 +1,32 @@
-import uvicorn
+import asyncio
 
+from loguru import logger
+
+from src.application.services.publisher import SignalPublisher
 from src.core.configurations.config import GlobalConfig
-from src.core.configurations.dishka import container
-from src.core.configurations.fastapi import get_app
+from src.core.configurations.dishka import create_container
+from src.core.configurations.faststream import create_faststream
 
 
-def main():
-    config: GlobalConfig = container.get(GlobalConfig)
-    app = get_app()
+async def main():
+    container = create_container()
+    app = await create_faststream(container)
+    publisher = await container.get(SignalPublisher)
 
-    uvicorn.run(app, host=config.app.host, port=config.app.port)
+    @app.after_startup
+    async def startup():
+        logger.info("Starting up")
+        config = await container.get(GlobalConfig)
+        publisher.start(config.worker.interval)
+
+    @app.after_shutdown
+    async def shutdown():
+        logger.info("Shutting down")
+        await publisher.stop()
+        await container.close()
+
+    await app.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
