802e2f55b06904b72c923f7f60f302195078c1d0
Maxim Vasilyev <mayxis@inbox.ru>
2026-02-16 23:59:55 +0300

    fix: FetchAndProcessLogsEvent typization

--- src/domain/dto/events.py
+++ src/domain/dto/events.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 
 
 class FetchAndProcessLogsEvent(BaseModel):
-    type: Literal["FETCH_AND_PROCESS_LOGS"] = Field("FETCH_AND_PROCESS_LOGS")
+    type: Literal["FETCH_AND_PROCESS_LOGS"] = "FETCH_AND_PROCESS_LOGS"
     triggered_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
 
     def model_dump(self, *args: Any, **kwargs: Any) -> dict[str, Any]:
